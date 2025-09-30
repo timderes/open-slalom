ca505a56ce01b0b679db858f5d4c48d535ec7120
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-09-30 14:17:46 +0200

    Refactor home page to index
    Renamed renderer/pages/home.tsx to renderer/pages/index.tsx and updated references to use the new index route. Standardized import statements to use double quotes and updated Electron main process to load the root URL instead of /home. Adjusted navigation in next.tsx to point to the new index route.

--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function NextPage() {
   return (
@@ -10,9 +10,9 @@ export default function NextPage() {
       </Head>
       <div>
         <p>
-          ⚡ Electron + Next.js ⚡ -<Link href="/home">Go to home page</Link>
+          ⚡ Electron + Next.js ⚡ -<Link href="/">Go to home page</Link>
         </p>
       </div>
     </React.Fragment>
-  )
+  );
 }
