8f44e5f88860696095552d4e80c0269435c35654
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-10-06 16:35:29 +0200

    Upgrade Mantine packages and add notifications
    Updated Mantine dependencies to version 8.3.3 and added @mantine/notifications to package.json. Imported notification styles and component in _app.tsx to enable notifications in the app.

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -2,13 +2,16 @@
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
 import "dayjs/locale/de";
 
 import type { AppProps } from "next/app";
+
 import { createTheme, MantineProvider } from "@mantine/core";
 import { DatesProvider } from "@mantine/dates";
 import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
@@ -17,6 +20,7 @@ const theme = createTheme({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
+      <Notifications limit={3} position="top-right" />
       <DatesProvider settings={{ locale: "de" }}>
         <ModalsProvider>
           <Component {...pageProps} />
