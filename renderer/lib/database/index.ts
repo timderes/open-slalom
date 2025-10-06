5093e8b25420b6b54bec187cd060b0e373806afa
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-10-06 11:06:32 +0200

    Add karts and trainings tables to database
    Introduces 'karts' and 'trainings' entity tables to the Dexie database schema and store definitions to support new data models.

--- renderer/lib/database/index.ts
+++ renderer/lib/database/index.ts
@@ -5,10 +5,14 @@ const LOWER_CASED_APP_NAME = APP_NAME.toLowerCase().replace(/\s+/g, "-");
 
 const database = new Dexie(LOWER_CASED_APP_NAME) as Dexie & {
   drivers: EntityTable<Driver, "uuid">;
+  karts: EntityTable<Kart, "uuid">;
+  trainings: EntityTable<Training, "uuid">;
 };
 
 database.version(1).stores({
   drivers: "&uuid",
+  karts: "&uuid",
+  trainings: "&uuid",
 });
 
 export default database;
