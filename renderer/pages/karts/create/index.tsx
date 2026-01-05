3a4d386720c047f038ef818fe425a62b050905be
Tim Deres <50623315+timderes@users.noreply.github.com>
2026-01-05 15:57:18 +0100

    feat: add kart creation page
    Introduces a new page for creating karts with form validation, notifications, and navigation. The form includes fields for name, engine, chassis, and type, and handles unsaved changes with a confirmation modal.

---
+++ renderer/pages/karts/create/index.tsx
@@ -0,0 +1,147 @@
+import Layout from "@/components/shared/Layout";
+import PageHeader from "@/components/shared/PageHeader";
+import database from "@/lib/database";
+import {
+  Button,
+  Container,
+  Group,
+  SegmentedControl,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { isNotEmpty, useForm } from "@mantine/form";
+import { modals } from "@mantine/modals";
+import { notifications } from "@mantine/notifications";
+import { useRouter } from "next/router";
+import { v4 as uuidv4 } from "uuid";
+
+const CreateKartPage = () => {
+  const router = useRouter();
+  const form = useForm<Kart>({
+    initialValues: {
+      name: "",
+      engine: "",
+      chassis: "",
+      type: "JKS",
+      history: {
+        laps: 0,
+        totalTrainingsSessions: 0,
+        totalTime: 0,
+        firstTraining: undefined,
+        lastTraining: undefined,
+      },
+      uuid: uuidv4(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+    validate: {
+      name: isNotEmpty("Dieses Feld darf nicht leer sein."),
+      engine: isNotEmpty("Dieses Feld darf nicht leer sein."),
+      chassis: isNotEmpty("Dieses Feld darf nicht leer sein."),
+    },
+
+    validateInputOnChange: true,
+  });
+
+  const handleCreateKart = () => {
+    database.karts.add(form.values).then(() => {
+      const { name } = form.values;
+
+      notifications.show({
+        // icon: <IconHelmet />,
+        title: "Kart angelegt",
+        message: `${name} wurde erfolgreich angelegt.`,
+        color: "green",
+      });
+
+      form.reset();
+      router.push("/karts");
+    });
+  };
+
+  const handleGoBack = () => {
+    if (!form.isDirty()) {
+      // Skip confirmation modal if form is not dirty
+      router.push("/karts");
+      return;
+    }
+
+    modals.openConfirmModal({
+      title: "Kart nicht anlegen?",
+      centered: true,
+      children: (
+        <Text>
+          Bereits eingetragende Informationen werden nicht gespeichert!
+        </Text>
+      ),
+      labels: { confirm: "Ja", cancel: "Nein" },
+      onConfirm: () => router.push("/karts"),
+    });
+  };
+
+  return (
+    <Layout currentRoute="/karts">
+      <Container my="sm">
+        <Stack>
+          <PageHeader title="Kart anlegen" />
+          <form
+            onSubmit={form.onSubmit(
+              () => handleCreateKart(),
+              (errors) => {
+                // Focus first invalid field
+                const getFirstErrorField = Object.keys(errors)[0];
+                form.getInputProps(getFirstErrorField).onFocus();
+              }
+            )}
+          >
+            <Stack>
+              <Group grow align="end">
+                <TextInput
+                  label="Kart"
+                  placeholder="Jugendkart #1"
+                  {...form.getInputProps("name")}
+                  key={form.key("name")}
+                />
+                <SegmentedControl
+                  color="blue"
+                  data={["JKS", "SKS"]}
+                  value={form.values.type}
+                  onChange={(value) =>
+                    form.setFieldValue("type", value as SlalomType)
+                  }
+                />
+              </Group>
+              <Group grow>
+                <TextInput
+                  label="Motor"
+                  placeholder="Honda GX-200"
+                  {...form.getInputProps("engine")}
+                  key={form.key("engine")}
+                />
+                <TextInput
+                  label="Chassis"
+                  placeholder="Mach1"
+                  {...form.getInputProps("chassis")}
+                  key={form.key("chassis")}
+                />
+              </Group>
+              <Group mt="xl">
+                <Button type="submit">Kart erstellen</Button>
+                <Button
+                  ms="auto"
+                  variant="subtle"
+                  onClick={() => handleGoBack()}
+                >
+                  Zurück
+                </Button>
+              </Group>
+            </Stack>
+          </form>
+        </Stack>
+      </Container>
+    </Layout>
+  );
+};
+
+export default CreateKartPage;
